db09e73f5bce2308e6d208919fe96098a80dae61
wrds <vw6cnhd9f7@privaterelay.appleid.com>
2026-07-25 02:40:04 +0800

    test(anki): 去掉 duplicate_scope_test 多余的 ankiconnect_service 直接 import

--- packages/hibiki_anki/test/duplicate_scope_test.dart
+++ packages/hibiki_anki/test/duplicate_scope_test.dart
@@ -4,7 +4,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/testing.dart';
 import 'package:hibiki_anki/hibiki_anki.dart';
-import 'package:hibiki_anki/src/ankiconnect/ankiconnect_service.dart';
 
 /// 查重范围（[AnkiDuplicateScope]）守卫。
 ///
